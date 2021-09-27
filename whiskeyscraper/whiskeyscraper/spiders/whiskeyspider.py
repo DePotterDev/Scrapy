c0ea7d751c148990578e7939dda279bc3e812c40
Laurens de Potter <depotter@pop-os.localdomain>
2021-09-26 23:34:30 -0300

    Finished it

--- whiskeyscraper/whiskeyscraper/spiders/whiskeyspider.py
+++ whiskeyscraper/whiskeyscraper/spiders/whiskeyspider.py
@@ -7,8 +7,21 @@ class WhiskeySpider(scrapy.Spider):
 
     def parse(self, response):
         for product in response.css('div.product-item-info'):
-            yield {
-                'name': product.css('a.product-item-link::text').get(),
-                'price': product.css('span.price::text').get().replace('£', ''),
-                'link': product.css('a.product-item-link').attrib['href']
-            }
+            try:
+                yield {
+                    'name': product.css('a.product-item-link::text').get(),
+                    'price': product.css('span.price::text').get().replace('£', ''),
+                    'link': product.css('a.product-item-link').attrib['href']
+                }
+            except:
+                yield {
+                    'name': product.css('a.product-item-link::text').get(),
+                    'price': 'sold out',
+                    'link': product.css('a.product-item-link').attrib['href']
+                }
+
+        next_page = response.css('a.action.next').attrib['href']
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+        
